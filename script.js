c7be46b1ca84dfb328db26c356b479c54a31118d
mkchitrada <mkchitrada@hotmail.com>
2023-11-09 00:23:09 +0800

    with reverse sort and edit

--- script.js
+++ script.js
@@ -1,71 +1,97 @@
-document.addEventListener('DOMContentLoaded', loadEntries);
+// Updated script with reverse sorting and edit feature
 
-let sortOrder = { name: 1, start: 1, end: 1, duration: 1 };
+let entries = [];
+let sortDirections = {0: true, 1: true, 2: true}; // true for ascending, false for descending
 
-function addEntry() {
-    let name = document.getElementById('studentName').value;
-    let start = document.getElementById('startDate').value;
-    let end = document.getElementById('endDate').value;
+function addOrUpdateEntry(index) {
+    const studentName = document.getElementById('studentName').value;
+    const startDate = document.getElementById('startDate').value;
+    const endDate = document.getElementById('endDate').value;
 
-    if (!name || !start || !end) {
-        alert('Please fill all fields.');
-        return;
+    const entryData = { studentName, startDate, endDate };
+
+    if (index != null) { // update existing entry
+        entries[index] = entryData;
+    } else { // add new entry
+        entries.push(entryData);
     }
 
-    let entries = JSON.parse(localStorage.getItem('vacationEntries') || '[]');
-
-    entries.push({ name, start, end });
-    localStorage.setItem('vacationEntries', JSON.stringify(entries));
-    loadEntries();
+    renderTable();
+    clearForm();
 }
 
-function loadEntries() {
-    let entries = JSON.parse(localStorage.getItem('vacationEntries') || '[]');
-    let tableBody = document.getElementById('vacationTable').getElementsByTagName('tbody')[0];
-    tableBody.innerHTML = '';
+function editEntry(index) {
+    const entry = entries[index];
+    document.getElementById('studentName').value = entry.studentName;
+    document.getElementById('startDate').value = entry.startDate;
+    document.getElementById('endDate').value = entry.endDate;
 
-    entries.forEach((entry, index) => {
-        let row = tableBody.insertRow();
-        let nameCell = row.insertCell(0);
-        let startCell = row.insertCell(1);
-        let endCell = row.insertCell(2);
-        let durationCell = row.insertCell(3);
-        let actionsCell = row.insertCell(4);
-
-        let startDate = new Date(entry.start);
-        let endDate = new Date(entry.end);
-        let duration = (endDate - startDate) / (1000 * 60 * 60 * 24);
-
-        nameCell.innerHTML = entry.name;
-        startCell.innerHTML = entry.start;
-        endCell.innerHTML = entry.end;
-        durationCell.innerHTML = duration + ' days';
-        actionsCell.innerHTML = `<button onclick="deleteEntry(${index})">Delete</button>`;
-
-        document.getElementById('studentName').value = '';
-        document.getElementById('startDate').value = '';
-        document.getElementById('endDate').value = '';
-    });
+    document.getElementById('submitBtn').innerText = 'Update';
+    document.getElementById('submitBtn').onclick = function() { addOrUpdateEntry(index) };
 }
 
 function deleteEntry(index) {
-    let entries = JSON.parse(localStorage.getItem('vacationEntries') || '[]');
     entries.splice(index, 1);
-    localStorage.setItem('vacationEntries', JSON.stringify(entries));
-    loadEntries();
+    renderTable();
+}
+
+function formatDate(dateString) {
+    const options = { day: '2-digit', month: 'short' };
+    return new Date(dateString).toLocaleDateString('en-US', options);
+}
+
+function clearForm() {
+    document.getElementById('studentName').value = '';
+    document.getElementById('startDate').value = '';
+    document.getElementById('endDate').value = '';
+    document.getElementById('submitBtn').innerText = 'Add';
+    document.getElementById('submitBtn').onclick = function() { addOrUpdateEntry() };
+}
+
+function renderTable() {
+    const tableBody = document.getElementById('tableBody');
+    tableBody.innerHTML = "";
+
+    entries.forEach((entry, index) => {
+        const row = `<tr>
+            <td>${entry.studentName}</td>
+            <td>${formatDate(entry.startDate)}</td>
+            <td>${formatDate(entry.endDate)}</td>
+            <td>
+                <button onclick="editEntry(${index})">Edit</button>
+                <button onclick="deleteEntry(${index})">Delete</button>
+            </td>
+        </tr>`;
+
+        tableBody.innerHTML += row;
+    });
 }
 
 function sortTable(columnIndex) {
-    let entries = JSON.parse(localStorage.getItem('vacationEntries') || '[]');
-    let key = columnIndex === 0 ? 'name' : columnIndex === 1 ? 'start' : 'end';
-    sortOrder[key] = -sortOrder[key];
+    const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
-    entries.sort((a, b) => {
-        return sortOrder[key] * ((new Date(a[key]) - new Date(b[key])) || a[key].localeCompare(b[key]));
-    });
+    let compare = (rowA, rowB) => {
+        let valA = getCellValue(rowA, columnIndex);
+        let valB = getCellValue(rowB, columnIndex);
+        if (columnIndex === 1 || columnIndex === 2) { // For date columns
+            valA = new Date(valA);
+            valB = new Date(valB);
+        }
+        return (valA > valB) ? 1 : (valA < valB) ? -1 : 0;
+    };
 
-    localStorage.setItem('vacationEntries', JSON.stringify(entries));
-    loadEntries();
+    let tableBody = document.getElementById('tableBody');
+    let rows = Array.from(tableBody.querySelectorAll('tr:nth-child(n+1)'));
+
+    if(sortDirections[columnIndex]) {
+        rows.sort(compare);
+    } else {
+        rows.sort((a, b) => -compare(a, b));
+    }
+
+    rows.forEach(tr => tableBody.appendChild(tr));
+    sortDirections[columnIndex] = !sortDirections[columnIndex]; // Toggle the direction
 }
 
-// You may need more complex handling for sorting by duration
+// Initial call to populate the table
+renderTable();
