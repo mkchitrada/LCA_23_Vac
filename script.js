e1067f802d6433e704827abfb9ec994c698b531f
mkchitrada <mkchitrada@hotmail.com>
2023-11-09 00:40:36 +0800

    init data added

--- script.js
+++ script.js
@@ -1,97 +1,153 @@
-// Updated script with reverse sorting and edit feature
+// script.js
 
-let entries = [];
-let sortDirections = {0: true, 1: true, 2: true}; // true for ascending, false for descending
-
-function addOrUpdateEntry(index) {
-    const studentName = document.getElementById('studentName').value;
-    const startDate = document.getElementById('startDate').value;
-    const endDate = document.getElementById('endDate').value;
-
-    const entryData = { studentName, startDate, endDate };
-
-    if (index != null) { // update existing entry
-        entries[index] = entryData;
-    } else { // add new entry
-        entries.push(entryData);
-    }
-
-    renderTable();
-    clearForm();
+// Utility function to format date as "dd-Mon"
+function formatDate(date) {
+    const options = { day: '2-digit', month: 'short' };
+    return new Date(date).toLocaleDateString('en-GB', options);
 }
 
+// Entry constructor function
+function createEntry(name, startDate, endDate) {
+    return {
+        studentName: name,
+        startDate: startDate,
+        endDate: endDate
+    };
+}
+
+// Function to parse and format entries for table display
+function parseEntriesForDisplay(entries) {
+    return entries.map(entry => ({
+        ...entry,
+        startDate: formatDate(entry.startDate),
+        endDate: formatDate(entry.endDate)
+    }));
+}
+
+// Pre-populated entries
+let entries = [
+    createEntry('Batra', '2023-11-08', '2023-11-18'),
+    createEntry('Sayesha', '2023-11-11', '2023-11-19'),
+    createEntry('Tanmai', '2023-11-10', '2024-01-01'),
+    createEntry('Rishta', '2023-11-15', '2023-11-30'),
+    createEntry('Darsh', '2023-11-14', '2023-11-26'),
+    createEntry('Shaktivel', '2023-11-27', '2023-12-03'),
+    createEntry('Monisha', '2023-12-05', '2024-01-03'),
+    createEntry('Anika', '2023-12-14', '2024-01-03'),
+    createEntry('Sophia', '2023-12-15', '2024-01-05'),
+    createEntry('Jiya', '2023-12-15', '2024-01-08'),
+    createEntry('Payal', '2023-12-16', '2024-01-07'),
+    createEntry('Anisha', '2023-12-18', '2024-01-08'),
+    createEntry('Ria', '2023-12-22', '2024-01-15'),
+    createEntry('Aagam', '2023-12-22', '2024-01-02'),
+    createEntry('Prisha', '2023-12-23', '2024-01-09'),
+];
+
+// Current edit index
+let editIndex = -1;
+
+// Function to add a new entry or update an existing one
+function addOrUpdateEntry() {
+    const nameInput = document.getElementById('studentName');
+    const startInput = document.getElementById('startDate');
+    const endInput = document.getElementById('endDate');
+    const name = nameInput.value.trim();
+    const start = startInput.value;
+    const end = endInput.value;
+
+    // Validate the inputs
+    if (!name || !start || !end) {
+        alert('Please fill in all fields');
+        return;
+    }
+
+    // Check if we're updating or adding a new entry
+    if (editIndex === -1) {
+        entries.push(createEntry(name, start, end));
+    } else {
+        entries[editIndex] = createEntry(name, start, end);
+        editIndex = -1; // Reset editIndex
+        document.getElementById('submitBtn').textContent = 'Add';
+    }
+
+    // Reset form
+    nameInput.value = '';
+    startInput.value = '';
+    endInput.value = '';
+
+    // Re-render the table
+    renderTable();
+}
+
+// Function to delete an entry
+function deleteEntry(index) {
+    if (confirm('Are you sure you want to delete this entry?')) {
+        entries.splice(index, 1);
+        renderTable();
+    }
+}
+
+// Function to edit an entry
 function editEntry(index) {
+    editIndex = index;
     const entry = entries[index];
     document.getElementById('studentName').value = entry.studentName;
     document.getElementById('startDate').value = entry.startDate;
     document.getElementById('endDate').value = entry.endDate;
-
-    document.getElementById('submitBtn').innerText = 'Update';
-    document.getElementById('submitBtn').onclick = function() { addOrUpdateEntry(index) };
+    document.getElementById('submitBtn').textContent = 'Update';
 }
 
-function deleteEntry(index) {
-    entries.splice(index, 1);
-    renderTable();
-}
-
-function formatDate(dateString) {
-    const options = { day: '2-digit', month: 'short' };
-    return new Date(dateString).toLocaleDateString('en-US', options);
-}
-
-function clearForm() {
-    document.getElementById('studentName').value = '';
-    document.getElementById('startDate').value = '';
-    document.getElementById('endDate').value = '';
-    document.getElementById('submitBtn').innerText = 'Add';
-    document.getElementById('submitBtn').onclick = function() { addOrUpdateEntry() };
-}
-
+// Function to render the table
 function renderTable() {
     const tableBody = document.getElementById('tableBody');
-    tableBody.innerHTML = "";
+    tableBody.innerHTML = ''; // Clear the table
 
-    entries.forEach((entry, index) => {
-        const row = `<tr>
-            <td>${entry.studentName}</td>
-            <td>${formatDate(entry.startDate)}</td>
-            <td>${formatDate(entry.endDate)}</td>
-            <td>
-                <button onclick="editEntry(${index})">Edit</button>
-                <button onclick="deleteEntry(${index})">Delete</button>
-            </td>
-        </tr>`;
+    // Parse entries for display
+    const displayEntries = parseEntriesForDisplay(entries);
 
-        tableBody.innerHTML += row;
+    // Add rows to the table
+    displayEntries.forEach((entry, index) => {
+        const row = tableBody.insertRow();
+        row.insertCell(0).textContent = entry.studentName;
+        row.insertCell(1).textContent = entry.startDate;
+        row.insertCell(2).textContent = entry.endDate;
+        
+        // Action buttons
+        const actionsCell = row.insertCell(3);
+        actionsCell.appendChild(createActionButton('Edit', () => editEntry(index)));
+        actionsCell.appendChild(createActionButton('Delete', () => deleteEntry(index)));
     });
 }
 
-function sortTable(columnIndex) {
-    const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
-
-    let compare = (rowA, rowB) => {
-        let valA = getCellValue(rowA, columnIndex);
-        let valB = getCellValue(rowB, columnIndex);
-        if (columnIndex === 1 || columnIndex === 2) { // For date columns
-            valA = new Date(valA);
-            valB = new Date(valB);
-        }
-        return (valA > valB) ? 1 : (valA < valB) ? -1 : 0;
-    };
-
-    let tableBody = document.getElementById('tableBody');
-    let rows = Array.from(tableBody.querySelectorAll('tr:nth-child(n+1)'));
-
-    if(sortDirections[columnIndex]) {
-        rows.sort(compare);
-    } else {
-        rows.sort((a, b) => -compare(a, b));
-    }
-
-    rows.forEach(tr => tableBody.appendChild(tr));
-    sortDirections[columnIndex] = !sortDirections[columnIndex]; // Toggle the direction
+// Function to create action buttons
+function createActionButton(text, action) {
+    const button = document.createElement('button');
+    button.textContent = text;
+    button.onclick = action;
+    return button;
 }
 
-// Initial call to populate the table
-renderTable();
+// Function to sort entries
+function sortEntries(column) {
+    entries.sort((a, b) => a[column].localeCompare(b[column]));
+    renderTable();
+}
+
+// Function to reverse sort entries
+function reverseSortEntries(column) {
+    entries.sort((a, b) => b[column].localeCompare(a[column]));
+    renderTable();
+}
+
+// Event listeners for sorting
+document.getElementById('sortName').addEventListener('click', () => sortEntries('studentName'));
+document.getElementById('reverseSortName').addEventListener('click', () => reverseSortEntries('studentName'));
+document.getElementById('sortStartDate').addEventListener('click', () => sortEntries('startDate'));
+document.getElementById('reverseSortStartDate').addEventListener('click', () => reverseSortEntries('startDate'));
+document.getElementById('sortEndDate').addEventListener('click', () => sortEntries('endDate'));
+document.getElementById('reverseSortEndDate').addEventListener('click', () => reverseSortEntries('endDate'));
+
+// Initial rendering of the table
+document.addEventListener('DOMContentLoaded', (event) => {
+    renderTable();
+});
