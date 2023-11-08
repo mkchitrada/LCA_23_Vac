75f083ec78ade59acb7ea7765dc6686f7fafc7bc
mkchitrada <mkchitrada@hotmail.com>
2023-11-08 23:09:36 +0800

    New version

--- script.js
+++ script.js
@@ -1,112 +1,97 @@
-document.addEventListener('DOMContentLoaded', loadEntries);
+document.addEventListener("DOMContentLoaded", function () {
+    const entryForm = document.getElementById("entry-form");
+    const entryList = document.getElementById("entry-ul");
+    const studentNameInput = document.getElementById("student-name");
+    const startDateInput = document.getElementById("start-date");
+    const endDateInput = document.getElementById("end-date");
+    const addEditButton = document.getElementById("add-edit-btn");
+    const cancelButton = document.getElementById("cancel-btn");
+    let editingIndex = -1;
 
-let sortOrder = { name: 1, start: 1, end: 1, duration: 1 };
+    addEditButton.addEventListener("click", function () {
+        const studentName = studentNameInput.value.trim();
+        const startDate = startDateInput.value.trim();
+        const endDate = endDateInput.value.trim();
 
-function addEntry() {
-    let name = document.getElementById('studentName').value;
-    let start = document.getElementById('startDate').value;
-    let end = document.getElementById('endDate').value;
+        if (!studentName || !startDate || !endDate) {
+            alert("Please fill in all fields.");
+            return;
+        }
 
-    if (!name || !start || !end) {
-        alert('Please fill all fields.');
-        return;
+        const entry = {
+            studentName,
+            startDate,
+            endDate,
+        };
+
+        if (editingIndex === -1) {
+            addEntry(entry);
+        } else {
+            editEntry(entry);
+        }
+
+        clearForm();
+    });
+
+    cancelButton.addEventListener("click", function () {
+        clearForm();
+    });
+
+    function addEntry(entry) {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `<span>${entry.studentName}</span> | <span>${entry.startDate}</span> to <span>${entry.endDate}</span> | <button class="edit-btn">Edit</button> | <button class="delete-btn">Delete</button>`;
+        entryList.appendChild(listItem);
+
+        const editButton = listItem.querySelector(".edit-btn");
+        const deleteButton = listItem.querySelector(".delete-btn");
+
+        editButton.addEventListener("click", function () {
+            fillFormForEdit(entry, listItem);
+        });
+
+        deleteButton.addEventListener("click", function () {
+            deleteEntry(listItem);
+        });
     }
 
-    let entries = JSON.parse(localStorage.getItem('vacationEntries') || '[]');
+    function fillFormForEdit(entry, listItem) {
+        studentNameInput.value = entry.studentName;
+        startDateInput.value = entry.startDate;
+        endDateInput.value = entry.endDate;
+        addEditButton.innerText = "Update Entry";
+        cancelButton.style.display = "inline";
+        editingIndex = Array.from(entryList.children).indexOf(listItem);
+    }
 
-    entries.push({ name, start, end });
-    localStorage.setItem('vacationEntries', JSON.stringify(entries));
-    loadEntries();
-}
+    function editEntry(entry) {
+        const listItem = entryList.children[editingIndex];
+        listItem.innerHTML = `<span>${entry.studentName}</span> | <span>${entry.startDate}</span> to <span>${entry.endDate}</span> | <button class="edit-btn">Edit</button> | <button class="delete-btn">Delete</button>`;
 
-function formatDate(dateString) {
-    const options = { day: '2-digit', month: 'short' };
-    return new Date(dateString).toLocaleDateString('en-US', options);
-}
+        const editButton = listItem.querySelector(".edit-btn");
+        const deleteButton = listItem.querySelector(".delete-btn");
 
-function loadEntries() {
-    let entries = JSON.parse(localStorage.getItem('vacationEntries') || '[]');
-    let tableBody = document.getElementById('vacationTable').getElementsByTagName('tbody')[0];
-    tableBody.innerHTML = '';
+        editButton.addEventListener("click", function () {
+            fillFormForEdit(entry, listItem);
+        });
 
-    entries.forEach((entry, index) => {
-        let row = tableBody.insertRow();
-        let nameCell = row.insertCell(0);
-        let startCell = row.insertCell(1);
-        let endCell = row.insertCell(2);
-        // let durationCell = row.insertCell(3);
-        let actionsCell = row.insertCell(4);
+        deleteButton.addEventListener("click", function () {
+            deleteEntry(listItem);
+        });
 
-        let startDate = new Date(entry.start);
-        let endDate = new Date(entry.end);
-        // let duration = (endDate - startDate) / (1000 * 60 * 60 * 24);
+        clearForm();
+    }
 
-        nameCell.innerHTML = entry.name;
-        startCell.innerHTML = formatDate(entry.start);
-        endCell.innerHTML = formatDate(entry.end);
-        // durationCell.innerHTML = duration + ' days';
-        actionsCell.innerHTML = `<button onclick="editEntry(${index})">Edit</button> <button onclick="deleteEntry(${index})">Delete</button>`;
+    function clearForm() {
+        studentNameInput.value = "";
+        startDateInput.value = "";
+        endDateInput.value = "";
+        addEditButton.innerText = "Add Entry";
+        cancelButton.style.display = "none";
+        editingIndex = -1;
+    }
 
-        document.getElementById('studentName').value = '';
-        document.getElementById('startDate').value = '';
-        document.getElementById('endDate').value = '';
-    });
-}
-
-function editEntry(index) {
-    let entries = JSON.parse(localStorage.getItem('vacationEntries') || '[]');
-    // Populate the form fields with the data from the entry to be edited
-    document.getElementById('studentName').value = entries[index].name;
-    document.getElementById('startDate').value = entries[index].start;
-    document.getElementById('endDate').value = entries[index].end;
-
-    // Update the Add button to Save
-    let addButton = document.querySelector('#form button');
-    addButton.textContent = 'Save';
-    addButton.onclick = function() {
-        saveEntry(index);
-    };
-}
-
-function saveEntry(index) {
-    let entries = JSON.parse(localStorage.getItem('vacationEntries') || '[]');
-    entries[index].name = document.getElementById('studentName').value;
-    entries[index].start = document.getElementById('startDate').value;
-    entries[index].end = document.getElementById('endDate').value;
-
-    localStorage.setItem('vacationEntries', JSON.stringify(entries));
-    loadEntries();
-
-    // Reset the button back to Add after saving
-    let addButton = document.querySelector('#form button');
-    addButton.textContent = 'Add';
-    addButton.onclick = addEntry;
-
-    document.getElementById('studentName').value = '';
-    document.getElementById('startDate').value = '';
-    document.getElementById('endDate').value = '';
-
-}
-
-
-function deleteEntry(index) {
-    let entries = JSON.parse(localStorage.getItem('vacationEntries') || '[]');
-    entries.splice(index, 1);
-    localStorage.setItem('vacationEntries', JSON.stringify(entries));
-    loadEntries();
-}
-
-function sortTable(columnIndex) {
-    let entries = JSON.parse(localStorage.getItem('vacationEntries') || '[]');
-    let key = columnIndex === 0 ? 'name' : columnIndex === 1 ? 'start' : 'end';
-    sortOrder[key] = -sortOrder[key];
-
-    entries.sort((a, b) => {
-        return sortOrder[key] * ((new Date(a[key]) - new Date(b[key])) || a[key].localeCompare(b[key]));
-    });
-
-    localStorage.setItem('vacationEntries', JSON.stringify(entries));
-    loadEntries();
-}
-
-// You may need more complex handling for sorting by duration
+    function deleteEntry(listItem) {
+        entryList.removeChild(listItem);
+        clearForm();
+    }
+});
